fd0b26299d6085c682e9ecf177ff3b054c452df7
Rahul Prabhakar <93304366+rahulprabhakar26@users.noreply.github.com>
2024-01-27 16:59:39 +0530

    Update CreateTable.java

--- src/com/kodnest/jdbc/CreateTable.java
+++ src/com/kodnest/jdbc/CreateTable.java
@@ -18,7 +18,7 @@ public class CreateTable {
 		// Connection Information
 		String url = "jdbc:mysql://localhost/kodnest5";
 		String username = "root";
-		String password = "Prabhakar@0626#";
+		String password = "root@123";
 
 		Connection connection = DriverManager.getConnection(url, username, password);
 		System.out.println(connection + "Connection success");
